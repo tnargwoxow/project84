31d2ccee9ec15b599aadb60e40a061ebfea4d988
Grant Moxom <tnargwoxow@gmail.com>
2014-10-01 22:16:27 +1000

    Got base audio integration working! Bug fixes required and optimisation - but FEAUTRE COMPELTE

--- AudienceAudioIntegrator.py
+++ AudienceAudioIntegrator.py
@@ -1,19 +1,27 @@
 __author__ = 'Administrator'
 print "HI"
-import os
 import subprocess
-import time
 import sys
-args="ssh ec2-user@54.210.51.206"+ " -tt -i "+ '"C:/Users/Administrator/Documents/UNI/Performing/Felicity Nicols - 1984/Technical/GrantCONNECT.pem"'+" "+ "sudo python /home/ec2-user/project84/showisk.py"
+import AudioPlayer
+args="ssh ec2-user@54.210.51.206"+ " -tt -i "+ '"C:/Users/Administrator/Documents/UNI/Performing/Felicity Nicols - 1984/Technical/GrantCONNECT.pem"'+" "+ "sudo python /home/ec2-user/project84/debugger.py"
 
 
 #ssh ec2-user@54.210.51.206"+ " -i "+ '"C:/Users/Administrator/Documents/UNI/Performing/Felicity Nicols - 1984/Technical/GrantCONNECT.pem"'+" sudo -s"+ " /home/ec2-user/project84/showisk.py"
 ssh=subprocess.Popen(args,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=True)
 while ssh.poll() is None:
     line = ssh.stdout.readline()
-    if "Playing audio file with channel" in line:
-        print "FUCK YEAH! THEYRE HERE!"
     sys.stdout.write(line)
+    if "ABLETON" in line:
+        stringtocut=line
+        print stringtocut + " This is the string to cut"
+        if "/audio/" in stringtocut:
+            cutstring=stringtocut[15:].rstrip()+".wav"
+        else:
+            cutstring=stringtocut[8:].rstrip()+".wav"
+        print cutstring+"THISISTHEFILE"
+        audiofile="C:\\Users\\Administrator\\Documents\\UNI\\Performing\\Felicity Nicols - 1984\\Technical\\AudioFiles\\Split\\"+cutstring
+        AudioPlayer.playInThread(audiofile)
+
 
 
 
